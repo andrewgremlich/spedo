58dccfacadde92439f0369a5d8517f10bb5d1241
andrewgremlich <andrew.gremlich@gmail.com>
2017-12-09 00:09:51 -0600

    Added buttons to stop geolocation and data upload.  Stopping geolocation will also cancel data upload, but not vice v versa.

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,40 @@ import lib from './lib.js'
 
 firebase.initializeApp(config)
 
-lib.query('#cancel')
-lib.query('#upload')
+let doGeolocation = true,
+  uploadData = true
+
+lib.query('#cancel').onclick = e => {
+  let target = e.target,
+    cancelIcon = target.src
+
+  if (cancelIcon.includes('x-circle.svg')) {
+    target.src = './icons/check-circle.svg'
+    doGeolocation = false
+    lib.query('#upload').click()
+    navigator.geolocation.clearWatch(id)
+  } else if (cancelIcon.includes('check-circle.svg')) {
+    target.src = './icons/x-circle.svg'
+    doGeolocation = true
+    lib.query('#upload').click()
+    id = navigator.geolocation.watchPosition(success
+                                            , err => console.warn('ERROR(' + err.code + '): ' + err.message)
+                                            , OPTIONS)
+  }
+}
+
+lib.query('#upload').onclick = e => {
+  let target = e.target,
+    cancelIcon = target.src
+
+  if (cancelIcon.includes('cloud.svg')) {
+    target.src = './icons/cloud-off.svg'
+    uploadData = false
+  } else if (cancelIcon.includes('cloud-off.svg')) {
+    target.src = './icons/cloud.svg'
+    uploadData = true
+  }
+}
 
 let id,
   database = firebase.database()
@@ -19,17 +51,16 @@ function success(pos) {
   lib.query('#speedContainer').innerHTML = ''
   lib.query('#speedContainer').innerHTML = temp
 
-  // firebase.database().ref('locationdata/').push({
-  //   timestamp: pos.timestamp,
-  //   altitude: coords.altitude,
-  //   heading: coords.heading,
-  //   latitude: coords.latitude,
-  //   longitude: coords.longitude,
-  //   speed: coords.speed
-  // })
-
-  // Stop watch geolocation watch
-  // navigator.geolocation.clearWatch(id)
+  if (uploadData) {
+    firebase.database().ref('locationdata/').push({
+      timestamp: pos.timestamp,
+      altitude: coords.altitude,
+      heading: coords.heading,
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      speed: coords.speed
+    })
+  }
 }
 
 id = navigator.geolocation.watchPosition(success
