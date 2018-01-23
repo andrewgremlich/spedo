5d042b9dc3f3cd797b3dbf82bda7407887883b01
andrewgremlich <andrew.gremlich@gmail.com>
2018-01-22 18:30:36 -0600

    Removed maps until further notice.

--- docs/js/cartographer.js
+++ docs/js/cartographer.js
@@ -10,4 +10,13 @@ function initMap() {
     ParmerRiataMap = new google.maps.Map(document.getElementById('ParmerRiataMap'), options),
     ParmerRiataLongMap = new google.maps.Map(document.getElementById('ParmerRiataLongMap'), options)
 
+  for (var i = 0; i < results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1], coords[0]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+
 }
