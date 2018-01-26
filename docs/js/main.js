610cd31ead3433a77d81757a6c00bfc5cd523d0c
andrewgremlich <andrew.gremlich@gmail.com>
2018-01-26 17:48:30 -0600

    Updated with run of Howard to Wells Branch and back to Howard again.
    Also made some organizational changes in main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,6 +5,7 @@ import parmerThroughRiata1 from './backup/parmerThroughRiata1.js'
 import parmerThroughRiata2 from './backup/parmerThroughRiata2.js'
 import parmerThroughRiata3 from './backup/parmerThroughRiata3.js'
 import parmerThroughRiataLong1 from './backup/ParmerThroughRiataLong.js'
+import HowardWellsBranch1 from './backup/HowardWellsBranch1.js'
 import dispenseStats from './statisticMethods.js'
 
 document.getElementById('remoteRepo').onclick = e => {
@@ -18,12 +19,18 @@ document.getElementById('refresh').onclick = e => {
 
 let parmerThroughRiata = [parmerThroughRiata1, parmerThroughRiata2, parmerThroughRiata3],
   parmerThroughRiataLong = [ parmerThroughRiataLong1 ],
+  HowardWellsBranch = [ HowardWellsBranch1 ],
   trialRunData = [ trialRunData1 ]
 
 makeGraph('Trial Run of Spedo App', document.getElementById("trialRunChart").getContext("2d"), trialRunData)
-makeGraph('Parmer Through Riata', document.getElementById("ParmerRiata").getContext("2d"), parmerThroughRiata)
-makeGraph('Parmer Through Riata Long', document.getElementById("ParmerRiataLong").getContext("2d"), parmerThroughRiataLong)
-
-dispenseStats('Parmer through Riata Run', document.getElementById('ParmerRiataStats'), parmerThroughRiata)
-dispenseStats('Parmer through Riata Long Run', document.getElementById('ParmerRiataLongStats'), parmerThroughRiataLong)
 dispenseStats('Trial Run', document.getElementById('trialRunStats'), trialRunData)
+
+
+makeGraph('Parmer Through Riata', document.getElementById("ParmerRiata").getContext("2d"), parmerThroughRiata)
+dispenseStats('Parmer through Riata Run', document.getElementById('ParmerRiataStats'), parmerThroughRiata)
+
+makeGraph('Parmer Through Riata Long', document.getElementById("ParmerRiataLong").getContext("2d"), parmerThroughRiataLong)
+dispenseStats('Parmer through Riata Long Run', document.getElementById('ParmerRiataLongStats'), parmerThroughRiataLong)
+
+makeGraph('Howard - Wells Branch - Howard', document.getElementById("HowardWellsBranch").getContext("2d"), HowardWellsBranch)
+dispenseStats('Howard - Wells Branch - Howard', document.getElementById('HowardWellsBranchStats'), HowardWellsBranch)
